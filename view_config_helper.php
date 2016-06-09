03983df3382bc5fb55b76809d414125bb865333f
Hill <hill@kimo.com>
2016-06-09 08:42:51 +0000

    add global VIEW and I18N

--- view_config_helper.php
+++ view_config_helper.php
@@ -22,7 +22,18 @@ class view_config_helper extends \PMVC\PlugIn
    public function onB4ProcessView() {
         $dot = \PMVC\plug('dotenv');
         $view = \PMVC\plug('view');
-        $viewConfigs = $dot->getArray('.env.view');
-        $view->set($viewConfigs);
+        $dotView = '.env.view';
+        if ($dot->fileExists($dotView)) { 
+            $viewConfigs = $dot->getArray($dotView);
+            $view->set($viewConfigs);
+        }
+        $globalView = \PMVC\getOption('VIEW');
+        if ($globalView) {
+            $view->set($globalView);
+        }
+        $i18n = \PMVC\getOption('I18N');
+        if ($i18n) {
+            $view->set($i18n);
+        }
    }
 }
