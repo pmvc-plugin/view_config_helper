341bdabe09beabc22f4f354c7fa99cc92a1f2915
Hill <hill@kimo.com>
2017-02-15 10:35:11 +0000

    Add feature switch

--- view_config_helper.php
+++ view_config_helper.php
@@ -39,13 +39,30 @@ class view_config_helper extends \PMVC\PlugIn
             $configs = array_replace_recursive($configs, ['I18N'=>$i18n]);
             \PMVC\option('set', 'I18N', null);
         }
-        $dotView = \PMVC\value($this,['.env'],'.env.view');
+        $dotView = \PMVC\get($this, '.env', '.env.view');
         if ($dot->fileExists($dotView)) {
             $configs = array_replace_recursive(
                 $configs,
                 $dot->getUnderscoreToArray($dotView)
             );
         }
+        
+        /*Clean disable section*/
+        $sections =& \PMVC\get($configs, 'section', []);
+        $features = \PMVC\getOption('features');
+        foreach ($sections as $sectionKey=>$section) {
+            if (empty($section['shouldRender'])) {
+                unset($sections[$sectionKey]);
+                continue;
+            }
+            $shouldRender = $section['shouldRender']; 
+            if (isset($features[$shouldRender]) &&
+                empty($features[$shouldRender])
+            ) {
+                unset($sections[$sectionKey]);
+            }
+        }
+
         if ($this['callback']) {
             $this['callback']($configs);
         }
